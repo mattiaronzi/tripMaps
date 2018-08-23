ee20e81d69a350573255455d1f5b2680c9dc31ea
ronzim <ro.ma.90@hotmail.it>
2018-08-23 19:01:52 +0200

    add index.html

--- index.js
+++ index.js
@@ -13,34 +13,34 @@ fs.createReadStream(inputZipPath)
   .pipe(unzip.Parse())
   .on('entry', function (entry) {
 
-    var fileName = entry.path;
-    var type = entry.type;
-    var extension = entry.path.split('.')[1];
+    var fileName  = entry.path;
+    var type      = entry.type;
+    var extension = path.extname(entry.path);
 
-    console.log(entry.type, entry.path, extension);
+    console.debug(entry.type, entry.path, extension);
 
-    if (type === 'File' && extension === 'json') {
+    if (type === 'File' && extension === '.json') {
 
       var fullPath = __dirname + '/output/' + path.dirname( fileName )
       fileName = path.basename( fileName )
       // mkdir.sync(fullPath)
       // entry.pipe(fs.createWriteStream( fullPath + '/' + fileName ))
-      entry.pipe(fs.createWriteStream( fileName ))
-
-      // FIXME understand how to avoid setTimeout usage
-      setTimeout(readJSON, 2000, fileName);
+      entry.pipe(
+        fs.createWriteStream(fileName)
+          .on('close', function(data){
+              console.log('close stream')
+              extractData(fileName);
+          })
+      )
 
     } else {
       entry.autodrain()
       // process.exit(0)
     }
-
   })
 
-function readJSON(fileJSON){
-  var rawData = fs.readJSONSync(fileJSON);
-  console.log(rawData);
+function extractData(jsonFile){
+  var rawData = fs.readJSONSync(jsonFile);
+  console.log('read JSON');
+  // TODO call function to extract data based on start & and time (input by argv)
 }
-
-
-// call function to extract data based on start & and time (input by argv)
